87e1a3940b444c30032cf62ecc17706449c4a23b
Vittor Emanoel <55211291+Vittor-Emanoel@users.noreply.github.com>
2024-10-11 18:47:35 -0300

    fix: add directives tailwindcss

--- consumer/src/App.tsx
+++ consumer/src/App.tsx
@@ -1,10 +1,13 @@
-import { Button } from "uiii-kit";
+import { Avatar, AvatarImage, Button } from "uiii-kit";
 
 export function App() {
-  return (
-    <div>
-      <Button className="border-t-indigo-50 bg-destructive">aawdawdwa</Button>
-      <h1 className="text-primary-foreground">Hello</h1>
-    </div>
-  );
+	return (
+		<div>
+			<Button>aawdawdwa</Button>
+			<Avatar>
+				<AvatarImage src="https://github.com/Vittor-Emanoel.png" />
+			</Avatar>
+			<h1 className="text-red-500">awdaw</h1>
+		</div>
+	);
 }
