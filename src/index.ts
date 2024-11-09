88955de868ca9f86ad7a10ae1307a7d627494070
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-11-09 16:53:59 -0300

    feat: add card component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./components/ui/Avatar";
 export * from "./components/ui/Button";
+export * from "./components/ui/Card";
 export * from "./components/ui/Dropdown-menu";
 export * from "./components/ui/Input";
