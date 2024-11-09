adb78bebf103a3a2ce1163e13c1d1839447dc046
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-11-09 14:07:09 -0300

    feat: mapping exports packages

--- uikit/.storybook/preview.ts
+++ uikit/.storybook/preview.ts
@@ -1,6 +1,6 @@
 import { withThemeByClassName } from "@storybook/addon-themes";
 import type { Preview, ReactRenderer } from "@storybook/react";
-import "../index.css";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
