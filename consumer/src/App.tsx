d86b098c502652b5c410dfb1a070142be414965f
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-11-09 14:06:58 -0300

    feat: mapping exports packages

--- consumer/src/App.tsx
+++ consumer/src/App.tsx
@@ -3,8 +3,8 @@ import { Button } from "uiii-kit";
 export function App() {
   return (
     <div>
-      <Button className="border-t-indigo-50 bg-destructive">aawdawdwa</Button>
-      <h1 className="text-primary-foreground">Hello</h1>
+      <Button className="border-t-indigo-50">awda</Button>
+      <h1 className="text-primary">Hello</h1>
     </div>
   );
 }
